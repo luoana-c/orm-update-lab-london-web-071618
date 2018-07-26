8d0423e69351b41ad98cbe1379e5bfac31d61a3b
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-26 16:32:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,6 +66,10 @@ class Student
   
   def update
     sql = <<-SQL
-      
+      UPDATE students
+      SET name = ?, grade = ?
+      WHERE id = ?
+      SQL
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end 
 end
